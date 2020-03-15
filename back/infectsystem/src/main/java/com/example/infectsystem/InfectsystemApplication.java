bc24f62b216f21bf7b0b6fb3a469ec50c8f567ba
Luckyzzzc <786621760@qq.com>
2020-03-15 22:02:16 +0800

    frame

---
+++ back/infectsystem/src/main/java/com/example/infectsystem/InfectsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.infectsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfectsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfectsystemApplication.class, args);
+    }
+
+}
---
+++ back/infectsystem/src/test/java/com/example/infectsystem/InfectsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infectsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfectsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
